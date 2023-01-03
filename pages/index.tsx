e8c7cd836fedbdd96ef347e82f111b87ee9c56ae
TerrniT <terrnit.megaman@gmail.com>
2023-01-03 18:05:29 +0500

    downgrade nextjs version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-import cat from "../assets/cat.png";
+
 import Notfound from "../components/notfound";
 
 const Home: NextPage = () => {
